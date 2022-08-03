3f3fb1c1912f96b68522e7cee40434efaa647506
Thien Chuong <thienchuong84@gmail.com>
2022-08-03 15:43:31 +0700

    test add v1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hoc AWS CI CD - 31/07/2022'));
+app.get('/', (req, res) => res.send('Hoc AWS CI CD - 31/07/2022 v1'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
